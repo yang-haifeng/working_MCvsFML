8e6a232c4dc818fa63963aa9fab9f0faba6d5619
Haifeng Yang <hfyangpku@gmail.com>
2016-12-06 23:59:15 -0500

    Modify comp.py to plot moew.

--- comp.py
+++ comp.py
@@ -6,6 +6,7 @@ from matplotlib.colors import LogNorm
 
 f = open("radmc3d/image.out")
 AU = 1.496e+13
+theta0 = 45./180*np.pi
 
 iformat = int(f.readline())
 im_nx, im_ny = np.array([int(i) for i in f.readline().split()])
@@ -40,21 +41,49 @@ angle1 = 0.5*np.arctan2(U1,-Q1)
 
 Ip1 = I1*p1
 
-R = np.sqrt(x*x+y*y)
-flag = R<160
+R = np.sqrt(x*x/(np.cos(theta0)**2)+y*y)
+flag = R<150
+
+plt.figure()
+plt.quiver(x[flag], y[flag], p1[flag]*np.cos(angle1[flag]), p1[flag]*np.sin(angle1[flag]))
+plt.savefig("pvec_radmc.pdf")
+#plt.colorbar()
+
+plt.figure()
+plt.tricontourf(x[flag], y[flag], p1[flag], 20)
+plt.colorbar()
+plt.title("Polarization Degree")
+plt.savefig("p_radmc.pdf")
 
 """
 plt.figure()
-plt.quiver(x[flag], y[flag], p1[flag]*np.cos(angle1[flag]), p1[flag]*np.sin(angle1[flag]))
+plt.imshow(Ip1, extent=extent)
+plt.colorbar()
 
+plt.figure()
+plt.imshow(I1, extent=extent)
+plt.colorbar()
+"""
+
+plt.figure()
+plt.tricontourf(x[flag], y[flag], Ip1[flag], 20)
+plt.colorbar()
+plt.title("Polarized Intensity")
+plt.savefig("Ip_radmc.pdf")
+
+"""
+plt.figure()
+plt.imshow(angle1)
+plt.colorbar()
+"""
+
+"""
 plt.figure()
 plt.hist(p1[flag], bins=50)
 """
 
 #####################################################
 # Below starts the formal solution data.
-
-theta0 = 45./180*np.pi
 
 data2 = np.loadtxt("fmlsol/result.dat")
 
@@ -71,11 +100,21 @@ theta2 = 0.5*np.arctan2(U2, Q2)
 
 kwargs = {"headlength":0, "headaxislength":0, "headwidth":1, "pivot":'middle'}#, "scale":scale, "width":width}
 
+plt.figure()
+plt.quiver(R2*np.cos(phi2)*np.cos(theta0), R2*np.sin(phi2), p2*np.cos(theta2), p2*np.sin(theta2), angles='uv', **kwargs)
+ax = plt.gca()
+
+plt.savefig("pvec_fml.pdf")
+
 """
 plt.figure()
-plt.quiver(R2*np.cos(phi2), R2*np.sin(phi2), p2*np.cos(theta2), p2*np.sin(theta2), angles='uv', **kwargs)
-ax = plt.gca()
-ax.set_aspect(1/np.cos(theta0))
+plt.tricontourf(R2*np.cos(phi2)*np.cos(theta0), R2*np.sin(phi2), theta2, 20)
+plt.colorbar()
+plt.show()
+"""
+
+#ax.set_aspect(1/np.cos(theta0))
+"""
 """
 
 R2.shape = 15, -1
@@ -83,12 +122,14 @@ phi2.shape = 15, -1
 p2.shape = 15, -1
 I2.shape = 15, -1
 Q2.shape = 15, -1
+theta2.shape = 15, -1
 
 phi2 = np.append(phi2.T, [[2*np.pi for _ in phi2[:,0]]],0).T
 R2 = np.append(R2.T, [[i for i in R2[:,0]]],0).T
 p2 = np.append(p2.T, [[i for i in p2[:,0]]],0).T
 I2 = np.append(I2.T, [[i for i in I2[:,0]]],0).T
 Q2 = np.append(Q2.T, [[i for i in Q2[:,0]]],0).T
+theta2 = np.append(theta2.T, [[i for i in theta2[:,0]]],0).T
 
 """
 plt.figure()
@@ -122,6 +163,26 @@ nRR = R2[f1]
 np2 = p2[f1]
 plt.plot(nRR, np2)
 
+plt.savefig("p_major.pdf")
+
+# Let's study angles along major axis
+plt.figure()
+
+f1 = x==0
+tny = y[f1]
+tnp = angle1[f1]
+f2 = tny>0
+nyy = tny[f2]
+np1 = tnp[f2]
+plt.plot(nyy, -np1)
+
+f1 = phi2==1.5708
+nRR = R2[f1]
+np2 = theta2[f1]
+plt.plot(nRR, np2)
+
+plt.savefig("pvecAngle_major.pdf")
+
 # Secondly, pQ along minor axis
 pQ1 = -Q1/I1
 pQ2 = Q2/I2
@@ -141,6 +202,8 @@ f1 = phi2==3.14159
 nRR = R2[f1] * np.cos(theta0)
 npQ2 = pQ2[f1]
 plt.plot(nRR, npQ2)
+
+plt.savefig("p_minor.pdf")
 
 # Let's do something with I's
 plt.figure()
@@ -166,4 +229,6 @@ plt.yscale('log')
 plt.legend(loc='best')
 plt.title("Total intensity")
 
-plt.show()
+plt.savefig("Itot.pdf")
+
+#plt.show()
